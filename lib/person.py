b54212771fff23970c6c355241f6a45aa1283b3d
Alin Silcock <at.silcock@gmail.com>
2024-09-10 06:14:03 -0600

    update

--- lib/person.py
+++ lib/person.py
@@ -4,4 +4,15 @@ class Person:
     # Class body goes here
 
     #Instance method definition
-    pass
+    def talk(self):
+        print("Hello World!")
+
+    def walk(self):
+        print("The person is walking.")
+
+gary = Person()
+gary.talk()
+
+alin = Person()
+alin.walk()
+
